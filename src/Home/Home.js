36896e9210336d177679f26b9f96e8e4730ce422
surya <suryamunaga89@gmail.com>
2024-02-08 11:30:29 +0530

    first commit

---
+++ src/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Home.module.css'
+export const Home = () => {
+    return (
+        <div>
+            <h1 className='mx-5 my-5 text-center'>Welcome...</h1>
+        </div>
+    )
+}
+
